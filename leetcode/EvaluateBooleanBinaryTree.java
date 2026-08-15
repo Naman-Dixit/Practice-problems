11931a4119fbc89a8847863976c1f119ec618f48
Naman Dixit <149253093+Naman-Dixit@users.noreply.github.com>
2026-08-15 22:20:39 +0530

    Create EvaluateBooleanBinaryTree.java

---
+++ leetcode/EvaluateBooleanBinaryTree.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public boolean evaluateTree(TreeNode root) {
+
+        // A leaf node's value IS the boolean result directly
+        if (root.left == null && root.right == null) {
+            return root.val == 1;
+        }
+
+        boolean leftResult = evaluateTree(root.left);
+        boolean rightResult = evaluateTree(root.right);
+
+        if (root.val == 2) {
+            return leftResult || rightResult;
+        } else {
+            return leftResult && rightResult;
+        }
+    }
+}
