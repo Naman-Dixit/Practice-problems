3fb9b3b3b1203a8fc18d51f3f64f57a3ad1c0690
Naman Dixit <149253093+Naman-Dixit@users.noreply.github.com>
2026-09-02 09:19:52 +0530

    Update Numberof1Bits.java

--- leetcode/Numberof1Bits.java
+++ leetcode/Numberof1Bits.java
@@ -22,3 +22,33 @@ class Solution {
         return count;
     }
 }
+
+
+
+public class Solution {
+    public int hammingWeight(int n) {
+        // 'count' keeps track of how many 1-bits we've found so far
+        int count = 0;
+
+        // Keep looping until all bits have been cleared (n becomes 0)
+        while (n != 0) {
+            // Brian Kernighan's trick: n & (n - 1) clears the LOWEST set bit.
+            //
+            // Why does this work?
+            // Subtracting 1 flips all bits from the lowest set bit downward.
+            // Example: n = 1100, n-1 = 1011
+            //          1100
+            //        & 1011
+            //        ------
+            //          1000   <- the lowest 1-bit (rightmost one) is cleared
+            //
+            n = n & (n - 1);
+
+            // Each time we clear a bit, that means we found one '1' bit
+            count++;
+        }
+
+        // Once n is 0, there are no more 1-bits left, so we're done
+        return count;
+    }
+}
